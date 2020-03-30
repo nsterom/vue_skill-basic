134550a9794815d5b33b6b8827f8f3aa7c94713c
Neil Lai <nsterom@gmail.com>
2020-03-30 10:58:53 +0800

    use v-link and call function in html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-        title: 'Hello World2!'
+        title: 'Hello World2!',
+        link: 'http://www.google.com'
     },
     methods: {
         changeTitle: function(event) {
             this.title = event.target.value
+        },
+        sayHello: function(event) {
+            return this.title
         }
     }
 })
