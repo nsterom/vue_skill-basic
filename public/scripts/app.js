6f6ce57fca923f1b0dc17beada98a3a2f3360cd7
mo141933 <lai_iekleong_mac@mail.notes.bank-of-china.com>
2020-03-31 08:39:36 +0800

    use v-html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,15 @@ new Vue({
     el: '#app',
     data: {
         title: 'Hello World2!',
-        link: 'http://www.google.com'
+        link: 'http://www.google.com',
+        finishedLink: '<a href="http://google.com">Google</a>'
     },
     methods: {
         changeTitle: function(event) {
             this.title = event.target.value
         },
         sayHello: function(event) {
+            this.title = 'in sayHello'
             return this.title
         }
     }
