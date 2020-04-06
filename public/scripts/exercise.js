4072a611285d6c1831db345c344881329fd25f00
Neil Lai <nsterom@gmail.com>
2020-04-06 22:12:22 +0800

    mouse event and keyboard event

--- public/scripts/exercise.js
+++ public/scripts/exercise.js
@@ -3,11 +3,18 @@ new Vue({
     data: {
         name: 'Neil',
         age: 30,
-        image: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'
+        image: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+        value: ''
     },
     methods: {
         getRandomNum: function(event) {
             return Math.random()
+        },
+        showAlert: function(event) {
+            alert('Alert!')
+        },
+        saveValue: function(event) {
+            this.value = event.target.value
         }
     }
 })
